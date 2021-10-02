8c290e7a740231b33f86499e5091195b2a875853
Wonmo Jung <jungcro@gmail.com>
2021-10-02 20:54:05 +0900

    init

---
+++ lib/builder.dart
@@ -0,0 +1,7 @@
+import 'package:code_gen_exp/generators/tlwr_page_generator.dart';
+import 'package:source_gen/source_gen.dart';
+import 'package:build/build.dart';
+
+Builder pageBuilder(BuilderOptions options) {
+  return LibraryBuilder(TLWRPageGenerator(), generatedExtension: '.g.dart');
+}
---
+++ lib/generators/tlwr_page_generator.dart
@@ -0,0 +1,57 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:code_gen_exp/annotations/tlwr_page.dart';
+import 'package:code_gen_exp/visitors/page_visitor.dart';
+import 'package:source_gen/source_gen.dart';
+import 'package:build/build.dart';
+
+class TLWRPageGenerator extends GeneratorForAnnotation<TLWRPage> {
+  @override
+  generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+    String className = element.displayName;
+    String path = buildStep.inputId.path;
+    // final optionOfLabel = optionOf(annotation.peek('label'));
+    // return optionOfLabel.fold(() => "//$className\n//$path\n//",
+    //     (label) => "//$className\n//$path\n//${label.stringValue}");
+
+    print("tlwr page generator");
+    print("//$className//$path//");
+    final visitor = PageVisitor();
+    final classBuffer = StringBuffer();
+
+    // 5
+    classBuffer.writeln('class $className extends ${visitor.className} {');
+
+    classBuffer.writeln('Map<String, dynamic> variables = {};');
+
+    classBuffer.writeln('$className() {');
+
+    for (final field in visitor.fields.keys) {
+      // remove '_' from private variables
+      final variable =
+          field.startsWith('_') ? field.replaceFirst('_', '') : field;
+
+      classBuffer.writeln("variables['${variable}'] = super.$field;");
+    }
+
+    classBuffer.writeln('}');
+    generateGettersAndSetters(visitor, classBuffer);
+    classBuffer.writeln('}');
+    return classBuffer.toString();
+  }
+
+  void generateGettersAndSetters(
+      PageVisitor visitor, StringBuffer classBuffer) {
+    for (final field in visitor.fields.keys) {
+      final variable =
+          field.startsWith('_') ? field.replaceFirst('_', '') : field;
+      classBuffer.writeln(
+          "${visitor.fields[field]} get $variable => variables['$variable'];");
+      classBuffer
+          .writeln('set $variable(${visitor.fields[field]} $variable) {');
+      classBuffer.writeln('super.$field = $variable;');
+      classBuffer.writeln("variables['$variable'] = $variable;");
+      classBuffer.writeln('}');
+    }
+  }
+}
