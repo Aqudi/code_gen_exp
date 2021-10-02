ac31deade0c6ecc3eb14393a39763b3dbac958df
Wonmo Jung <jungcro@gmail.com>
2021-10-03 07:27:17 +0900

    add more builder factories

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,3 +1,5 @@
+import 'package:code_gen_exp/builders/locating_builder.dart';
+import 'package:code_gen_exp/builders/node_aggregating_builder.dart';
 import 'package:code_gen_exp/generators/tlwr_page_generator.dart';
 import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
@@ -5,3 +7,8 @@ import 'package:build/build.dart';
 Builder pageBuilder(BuilderOptions options) {
   return PartBuilder([TLWRPageGenerator()], '.tlwr.dart');
 }
+
+Builder locatingBuilder(BuilderOptions options) => LocatingBuilder();
+
+Builder nodeAggregatingBuilder(BuilderOptions options) =>
+    NodeAggregatingBuilder();
